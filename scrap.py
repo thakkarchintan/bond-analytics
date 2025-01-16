0644972b8362ea936f3b4e0ae4dfb724e301d298
Pratik Shah <pratik.ashah676@gmail.com>
2025-01-16 11:43:26 +0530

    Add log file

--- scrap.py
+++ scrap.py
@@ -49,7 +49,7 @@ def update_last_run_date(log_file="last_run_date.txt"):
     with open(log_file, "w") as file:
         file.write(datetime.today().strftime("%Y-%m-%d"))
 
-def update_data(url, tool, append_dates=False , retries=3):
+def update_data(url, tool, append_dates=False , retries=5):
     with sync_playwright() as p:
         # Launch browser with additional arguments and settings
         browser = p.chromium.launch(headless=True, args=[
@@ -103,6 +103,7 @@ def update_data(url, tool, append_dates=False , retries=3):
                     time.sleep(5)  # Wait before retrying
                 else:
                     print("Max retries reached. Could not load the page.")
+                    return False
 
         # Parse the page content
         html = page.content()
