4b059043ded98db1a2b9d1b356c6d5772a073043
Pratik Shah <pratik.ashah676@gmail.com>
2025-01-20 15:50:55 +0530

    Add some changes

--- common.py
+++ common.py
@@ -4,11 +4,14 @@ import os
 
 load_dotenv()
 
+client_secret_path = "/etc/secrets/client_secret.json"
+
+
 allowed_users = os.getenv("ALLOWED_USERS").split(",")
 
 authenticator = Authenticator(
     allowed_users=allowed_users,
     token_key=os.getenv("TOKEN_KEY"),
-    secret_path="client_secret.json",
+    secret_path=client_secret_path,
     redirect_uri="http://localhost:8501",
 )
