35db963a7719eebb7fd068387a3ab6965d4fe7dd
Pratik Shah <pratik.ashah676@gmail.com>
2025-04-02 12:41:35 +0530

    Scrap updated for ec2

--- scrap.py
+++ scrap.py
@@ -265,9 +265,9 @@ try:
     file_path = "Final.xlsx"
     today = datetime.now()
     formatted_date = today.strftime("%d-%m-%Y")
-    subprocess.run(["python", "WeekendRm.py"])
+    subprocess.run(["/usr/bin/python3", "WeekendRm.py"])
     logging.info("Weekends Removed")
-    subprocess.run(["python","interpolater.py"])
+    subprocess.run(["p/usr/bin/python3","interpolater.py"])
     logging.info("Completed linear interpolation")
     # Add all changes
     subprocess.run(["git", "add", file_path], cwd=repo_path, check=True)
